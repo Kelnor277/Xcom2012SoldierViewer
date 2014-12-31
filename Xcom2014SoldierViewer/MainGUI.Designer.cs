80291e123a54ee33298a9f04c72ad0ec7ed5bf8c
Kelnor277 <jonesj3599@gmail.com>
2014-12-30 20:25:04 -0600

    Reverse engineered save file. Now parses save file and loads soldiers into a class. TODO: Figure out what each property represents in game. Write methods to take data and produce meaniful outputs (IE. Iterate the perks array and create a list of perks the soldier has.) Create a bitchin GUI.

--- Xcom2014SoldierViewer/MainGUI.Designer.cs
+++ Xcom2014SoldierViewer/MainGUI.Designer.cs
@@ -28,12 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(108, 478);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.listView1.Location = new System.Drawing.Point(68, 58);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(451, 377);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // MainGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(732, 551);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Name = "MainGUI";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainGUI_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
     }
 }
 
--- Xcom2014SoldierViewer/MainGUI.cs
+++ Xcom2014SoldierViewer/MainGUI.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Xcom2014SoldierViewer
 {
@@ -15,6 +16,31 @@ namespace Xcom2014SoldierViewer
         public MainGUI()
         {
             InitializeComponent();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            string saveFileName = Path.Combine(new string[] {Directory.GetCurrentDirectory(), Path.GetFileName(openFileDialog1.FileName)});
+            SaveReader.decompress(openFileDialog1.FileName, saveFileName);
+        }
+
+        private void MainGUI_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            string saveFileName = Path.Combine(new string[] { Directory.GetCurrentDirectory(), Path.GetFileName(openFileDialog1.FileName) });
+            SaveReader.decompress(openFileDialog1.FileName, saveFileName);
+            List<XGStrategySoldier> Roster = SaveReader.parseSoldiers(saveFileName);
         }
     }
 }
